a78fd40c89fbd4b9c5fe7689994af9b56a69c8ef
jaidesai1906 <jai.desai_ug22@ashoka.edu.in>
2020-10-17 13:21:32 +0530

    Added features-
    -Email Confirmation
    - Fixed bugs in popup

--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import json
+from django import forms
 from .models import *
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from django.conf import settings
+from django.core.mail import send_mail 
 
 # Create your views here.
 def store(request):
@@ -92,21 +95,46 @@ def updateItem(request):
 
     return JsonResponse('Item was added', safe=False)
     
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=20, help_text='Enter your first name')
+    email = forms.EmailField(max_length=64, help_text='Enter a valid email address')
+    
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # I've tried both of these 'fields' declaration, result is the same
+        # fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = UserCreationForm.Meta.fields + ('first_name', 'email',)
+
+
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
+            first_name = form.data.get('first_name')
+            email = form.cleaned_data.get('email')
             messages.success(request, f"New Account Created: {username}")
             login(request, user)
+
+            subject = 'welcome to Jar-3D-Wale!!'
+            message = f'Hi {first_name},\nThank you for registering on JARThreeDeeWale!! \nEnjoy shopping on our site!'
+            email_from = settings.EMAIL_HOST_USER 
+            recipient_list = [email, ] 
+            send_mail( subject, message, email_from, recipient_list ) 
+
             return redirect("/")
+
+            
+
+            
         else:
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
             
 
-    form = UserCreationForm
+    form = SignUpForm
     context = {'form': form}
     return render(request, "store/register.html", context)
                     
